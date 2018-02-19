2f3f59ebc80de94ba97aa4bb32914aca6cc08f6b
blainmorin <blainmorin@brown.edu>
2018-02-18 21:33:31 -0500

    function working

--- tide_scrape_function.R
+++ tide_scrape_function.R
@@ -1,24 +1,33 @@
 library(XML)
-
-tideurl = "http://tides.mobilegeographics.com/calendar/month/4082.html?y=2016&m=5&d=20"
-
-tidetable = readHTMLTable(tideurl, header = TRUE, as.data.frame = TRUE)
-
-
-tidetable = as.data.frame(tidetable$"NULL")
-
-tidetable
-
+library(tidyverse)
 
 scrapetide = function( year, month) {
   
-  tideurl = paste("http://tides.mobilegeographics.com/calendar/month/4082.html?y=", year, "&m=", month, "&d=20")
+  tideurl = paste("http://tides.mobilegeographics.com/calendar/month/4082.html?y=", year, "&m=", month, "&d=20", sep = "")
   tidetable = readHTMLTable(tideurl, header = TRUE, as.data.frame = TRUE)
   tidetable = as.data.frame(tidetable$"NULL")
   return(tidetable)
+  Sys.sleep(6+runif(1)*20)
   
   
 }
 
-scrapetide(2015, 4)
+###Set years and months here
+
+years = 2014:2016
+months = 1:12
+
+tester = data.frame()
+
+for (i in (1:length(years))){
+  for (j in (1:length(months))){
+    temp = scrapetide(years[i], months[j])
+    temp$year = years[i]
+    temp$month = months[j]
+    tester = rbind(tester, temp)
+  }
+  
+}
+
+
 
